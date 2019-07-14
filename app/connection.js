9c2bf38bec7f811385783ca933f5e53c8fc295f4
Zahid <zahidin98@gmail.com>
2019-07-14 13:28:24 +0700

    fix code again

--- app/connection.js
+++ app/connection.js
@@ -7,7 +7,7 @@ export const connect = Knex({
     client:process.env.KNEX_CLIENT,
     connection:{
         host:process.env.HOST,
-        username:process.env.DB_USERNAME,
+        user:process.env.DB_USERNAME,
         password:process.env.DB_PASSWORD,
         database:process.env.DB_NAME,
         port:process.env.DB_PORT
